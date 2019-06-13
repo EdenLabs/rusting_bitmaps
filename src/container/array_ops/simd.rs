bd5347c35dfa50b9b175a8fd08a8d63d107fc547
Techgeek1 <austin.rife@gmail.com>
2019-06-13 09:40:28 -0700

    Started SIMD refactor

---
+++ src/container/array_ops/simd.rs
@@ -0,0 +1,20 @@
+
+/// Perform the set union operation between `a` and `b` outputting the results into `out`
+pub unsafe fn or(a: *const u16, b: *const u16, out: *mut u16) {
+
+}
+
+/// Perform the set intersection operation between `a` and `b` outputting the results into `out`
+pub unsafe fn and(a: *const u16, b: *const u16, out: *mut u16) {
+
+}
+
+/// Perform the set difference operation between `a` and `b` outputting the results into `out`
+pub unsafe fn and_not(a: *const u16, b: *const u16, out: *mut u16) {
+
+}
+
+/// Perform the set symmetric difference operation between `a` and `b` outputting the results into `out`
+pub unsafe fn xor(a: *const u16, b: *const u16, out: *mut u16) {
+
+}
